f9416fbe72596372b8d72b49a35cef237a53a5fe
Muhammad Furqan <furqan.khanzada@gmail.com>
2024-09-03 23:42:37 +0500

    chore: add theme and init landing page

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList, navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu";
+import ToggleThemeMod from "@/components/toggle-theme-mod";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Link href="/" legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Home
+                </NavigationMenuLink>
+              </Link>
+              <a href="https://www.upwork.com/fl/muhammadf447" target="_blank">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Upwork
+                </NavigationMenuLink>
+              </a>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="flex flex-1 justify-end">
+          <ToggleThemeMod />
+        </div>
+      </div>
+    </header>
+  )
+}
